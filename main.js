452a882c547656340aea2cfcf66f121d755c35aa
daeneegen <daeneegen@gamil.com>
2024-04-22 17:16:16 +0300

    point

--- main.js
+++ main.js
@@ -2,9 +2,29 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYXRob24yMDI0LXRlYW02IiwiYSI6ImNsdmFtNjZnM
 const map = new mapboxgl.Map({
  container: 'map', // container ID
  style: 'mapbox://styles/mapbox/streets-v12', // style URL
- center: [-74.5, 40], // starting position [lng, lat]
+ center: [27.5667, 53.9], // starting position [lng, lat]
  zoom: 9, // starting zoom
 });
+
+map.on('load', () => {
+    map.addSource('places', {
+        'type': 'geojson',
+        'data': './restaurants.geojson'
+	});
+	map.addLayer({
+		'id': 'places',
+		'type': 'circle',
+		'source': 'places',
+		'paint': {
+			'circle-color': '#4264fb',
+			'circle-radius': 6,
+			'circle-stroke-width': 2,
+			'circle-stroke-color': '#ffffff'
+		}
+});
+
+});
+
 
 function checkForm (forma){
 	var Name = forma.name.value;
