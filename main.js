b33edd527a643d10fc5ba305727c52248273a817
Loentrin <56451550+Loentrin@users.noreply.github.com>
2024-04-22 15:17:29 +0300

    add map

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYXRob24yMDI0LXRlYW02IiwiYSI6ImNsdmFtNjZnMDE1bDQyanJyeTRjbnZhNHoifQ.QnsGQhCGixoqajiUos7vfg';
+const map = new mapboxgl.Map({
+ container: 'map', // container ID
+ style: 'mapbox://styles/mapbox/streets-v12', // style URL
+ center: [-74.5, 40], // starting position [lng, lat]
+ zoom: 9, // starting zoom
+});
+
 function checkForm (forma){
 	var Name = forma.name.value;
 	var Password = forma.pass.value;
