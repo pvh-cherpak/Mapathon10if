58d02d3b90e929d4317f596c1ac37d351b87d731
Loentrin <56451550+Loentrin@users.noreply.github.com>
2024-04-23 15:13:07 +0300

    a

--- main.js
+++ main.js
@@ -92,13 +92,13 @@ map.on('load', () => {
 		}
 	});
 
-	map.on('click', (event) => {  
+	/*map.on('click', (event) => {  
  		// If the user clicked on one of your markers, get its information.  
  		const features = map.queryRenderedFeatures(event.point, {  
  			layers: ['fastfood', 'pizza', 'sushi', 'shaurma'] // replace with your layer name  
  		});
 		console.log(features)
- 	});  
+ 	});  */
 });
 
 
