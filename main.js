87e640e1eacad91d17494d5481987b4edd789eda
Loentrin <56451550+Loentrin@users.noreply.github.com>
2024-04-24 10:19:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -16,17 +16,6 @@ var popup = new mapboxgl.Popup();
 
 function loadMap(){
 	// Add geolocate control to the map.
-	map.addControl(
-		new mapboxgl.GeolocateControl({
-			positionOptions: {
-				enableHighAccuracy: true
-			},
-			// When active the map will receive updates to the device's location as it changes.
-			trackUserLocation: true,
-			// Draw an arrow next to the location dot to indicate which direction the device is heading.
-			showUserHeading: true
-		})
-	);
 	map.addSource('restarans_sourse', {
 		'type': 'geojson',
 		'data': './restaurants.geojson'
@@ -73,6 +62,17 @@ function loadMap(){
 }
 
 map.on('load', () => {
+	map.addControl(
+		new mapboxgl.GeolocateControl({
+			positionOptions: {
+				enableHighAccuracy: true
+			},
+			// When active the map will receive updates to the device's location as it changes.
+			trackUserLocation: true,
+			// Draw an arrow next to the location dot to indicate which direction the device is heading.
+			showUserHeading: true
+		})
+	);
 	loadMap()
 });
 
